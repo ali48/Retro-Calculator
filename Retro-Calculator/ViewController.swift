dbdda372fe853ff8bc4e4a91230a101aaa9b4c31
Muhammad Ali <muhammadali@Muhammads-MacBook-Air.local>
2016-02-20 15:25:59 -0500

    Code added

--- Retro-Calculator/ViewController.swift
+++ Retro-Calculator/ViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    @IBOutlet weak var counterLbl: UILabel!
+    var runningValue = ""
+    var leftValue = ""
+    var rightValue = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!) {
+        runningValue += "\(btn.tag)"
+        counterLbl.text = runningValue
     }
 
+    @IBAction func onDivision(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
 
+    @IBAction func onMultiply(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onAdd(sender: AnyObject) {
+       processOperation(Operation.Add)
+    }
+    
+    @IBAction func onSubtract(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onEqual(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    
+    func processOperation(op: Operation) {
+        
+        if currentOperation != Operation.Empty {
+            //Do Math
+        }else {
+            // This is first time operator is pressed
+            leftValue = runningValue
+            runningValue = ""
+            currentOperation = op
+            
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
 
